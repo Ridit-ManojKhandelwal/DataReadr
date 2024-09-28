293bdae86af46f24eaa468bdd005e967e69bbcc9
Manoj Khandelwal <tr.manojkhandelwal@gmail.com>
2024-09-28 21:38:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.11.2'
+VERSION = '0.11.3'
 DESCRIPTION = 'DataReadr GUI Interface simplifies the process of adding Pandas code into Python scripts. Through a user-friendly GUI, users can easily select and insert Pandas functions, automating repetitive tasks and enhancing productivity in data analysis projects.'
 LONG_DESCRIPTION = 'An Application that simplifies the process of creating dataframes in python with GUI'
 
